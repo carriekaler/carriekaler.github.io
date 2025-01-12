dbf78acef52e1ed36ece619eed0189c692400f55
Carrie Kaler <carriekaler@gmail.com>
2025-01-12 16:35:59 -0600

    Create Band Name Generator.py

---
+++ Day 1 Task/Band Name Generator.py
@@ -0,0 +1,13 @@
+# Create Greeting for program
+print("Welcome to the Band Name Generator Project")
+print("Day 1 Project for 100 Days of Code - The complete Python Pro Bootcamp")
+print("\n")
+
+# Ask user for the city that they grew up in and store it as a variable
+user_city = input("What city did you grow up in?: ")
+
+# Ask the user for the name of a pet and store it in a variable.
+user_pet = input("What is the name of one of your pets?: ")
+
+# Combine the name of their city and pet and show them their band name.
+print("Your band name is: " + user_city + " " + user_pet)
